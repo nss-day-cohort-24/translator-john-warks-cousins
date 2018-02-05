f5f76906dd3fffdf1149646d9d35dcfb8e986adc
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-02-05 15:19:09 -0600

    fix files to try to get grunt going

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("main js is linked");
+
+let langModule = require("./language");
+console.log("Language Module: ", langModule);
+console.log("here's another console log at the end");
