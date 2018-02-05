e75701a75735ba3f5140411450a67fe6db35b766
Dylan B <giglett21@gmail.com>
2018-02-05 13:31:03 -0800

    addded german.js

--- js/language.js
+++ js/language.js
@@ -1,7 +1,7 @@
 "use strict";
-let russia = require("../russia");
-let german = require("../german");
-let french = require("../french");
+let russia = require("./russia");
+let german = require("./german");
+let french = require("./french");
 let translator = {
     russia,
     german,
