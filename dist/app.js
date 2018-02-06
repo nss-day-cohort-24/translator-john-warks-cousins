9e3863b8139dfc4460671b572143583e872373a4
Arthur Smith <mirandasimpson@Mirandas-MacBook-Air.local>
2018-02-06 12:31:29 -0600

    the french function that is correct

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,21 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 console.log("Let's translate to french!");
-var frenchLang = {"merry":"joyeux", "christmas":"noel", "and":"et", "happy":"bonne", "new year":"année"};
-var text = document.getElementById("text-field");
-var translate = document.getElementById("translation");
+var frenchLang = {merry:"joyeux", christmas:"noel", and:"et", happy:"bonne", newyear:"année"};
+// var text = document.getElementById("text-field");
+// var translate = document.getElementById("translation");
 
-function translateTo(french){
+function frenchTranslator(words){
+    console.log("words", words);
     for (let item in frenchLang){
-        if (text.value === item){
-            console.log(text.value);
-            translate.innerHTML = frenchLang[item];
+        if (words == item){
+            console.log(frenchLang[`${item}`]);
+            document.getElementById("translation").innerHTML = frenchLang[`${item}`];
         }
     }
 }
 
-module.exports = translateTo(frenchLang);
+module.exports = frenchTranslator;
 
 // function french(){
 //     return french;
@@ -68,6 +69,22 @@ let translator = {
     spanish
 };
 
+
+document.getElementById("btn-trans").addEventListener("click", function(){
+    var x = document.getElementById("langOptions").selectedIndex;
+    console.log("click", x, document.getElementsByTagName("option")[x].value);
+    
+        if (document.getElementsByTagName("option")[x].value === 'french'){
+            console.log('got here');
+            translator.french(document.getElementById("text-field").value);
+    }
+});
+// document.getElementById("btn-trans").addEventListener("click", function(){
+//     if (document.getElementsByTagName("option")[x].value === 'spanish') {
+//         console.log(translator.spanish);
+//         return translator.spanish;
+//     }
+// });
 module.exports = translator;
 },{"./french":1,"./german":2,"./russian":5,"./spanish":6}],4:[function(require,module,exports){
 "use strict";
@@ -98,22 +115,27 @@ module.exports = translateTo(russiaLang);
 },{}],6:[function(require,module,exports){
 "use strict";
 
-console.log("hola mundo");
+//console.log("hola mundo");
 
 var spanishLang = {"merry":"alegre", "christmas":"Navidad", "and":"y", "happy":"contento", "new":"nuevo", "year":"año"};
 
 var text = document.getElementById("text-field");
 var translate = document.getElementById("translation");
 
-function translateTo(spanish){
+function translateTo(spanish){    
     for (let item in spanishLang){
-        if (text.value === item){
-            console.log(text.value);
-            translate.innerHTML = spanishLang[item];
+        switch (spanishLang) {
+            case text.value == item:
+                translate.innerHTML = spanishLang[item];
+                break;
         }
     }
 }
-
-
+// var x = document.getElementById("langOptions").selectedIndex;
+// if (document.getElementsByTagName("option")[x].value === 'spanish') {
+//     document.getElementById("btn-trans").addEventListener("click", translateTo() );
+// }
+    
+    
 module.exports = translateTo(spanishLang);
 },{}]},{},[4]);
