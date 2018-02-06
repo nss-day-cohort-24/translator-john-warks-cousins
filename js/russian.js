4f450f3af60c7fc947b5d3d9a74e12d5a5b1259d
Arthur Smith <mirandasimpson@Mirandas-Air.attlocal.net>
2018-02-05 20:18:33 -0600

    until tomorow

--- js/russian.js
+++ js/russian.js
@@ -1,8 +1,18 @@
 "use strict";
 console.log("hello russia");
-function russia(){
-    return russia;
+var russiaLang = {"merry":"veselly", "christmas":"rozhdestvo", "and":"a takzhe", "happy":"schastlivyy", "new":"novyy", "year":"god"}
+
+var text = document.getElementById("text-field");
+var translate = document.getElementById("translation");
+
+function translateTo(russian){
+    for (let item in russiaLang){
+        if (text.value === item){
+            console.log(text.value);
+            translate.innerHTML = russiaLang[item];
+        }
+    }
 }
 
 
-module.exports = russia;
+module.exports = translateTo(russian);
